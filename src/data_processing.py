62508062dff7ff3036a486f4b9a78baa8e028d9d
Lindsey Eggleston <lindsey.eggleston@gmail.com>
2017-06-09 13:57:01 -0600

    add text_to_vocab funct

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,8 @@
 import numpy as np
 import os
+from keras.preprocessing.text import text_to_word_sequence, one_hot
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import OrderedDict, defaultdict, namedtuple
 
 def file_to_text(filepath):
     if os.path.isfile(filepath):
@@ -9,11 +12,34 @@ def file_to_text(filepath):
             text = t.read().lower()
     return text
 
-def text_to_vocab(text):
-    pass
+def text_to_vocab(text, vocab_length=None):
+    '''
+    Learns the vocabulary within a text and refines the length to the most
+    frequently used words
+
+    Parameters:
+    -----------
+    text: LIST/ARRAY - iterable which yields a string of text
+    vocab_length: INT - num of words in vocab
+
+    Returns:
+    --------
+    a vocab dictionary of specified length if vocab_length not None
+    '''
+    vectorizer = TfidfVectorizer(max_features=vocab_length)
+    X = vectorizer.fit_transform(text)
+    vocab = vectorizer.vocabulary_
+    return vocab
 
 def text_to_sequence(text):
     pass
 
 def create_minibatch(batchsize):
     pass
+
+if __name__ == '__main__':
+    with open('../data/SOIF/AGameOfThrones.txt') as f:
+        text = f.read()
+        raw_data = text.split('\n')
+    vocab = text_to_vocab(raw_data, 5000)
+    print('Length: ', len(vocab))
