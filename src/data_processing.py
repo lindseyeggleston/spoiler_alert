3b5441b1b3d5a7aa2a1003f8d02004dba26209e3
Lindsey Eggleston <lindsey.eggleston@gmail.com>
2017-06-09 18:12:27 -0600

    add minibatch funct

--- src/data_processing.py
+++ src/data_processing.py
@@ -34,10 +34,39 @@ def text_to_vocab(text, vocab_length=None):
 def text_to_sequence(text):
     pass
 
-def create_minibatch(text, batch_size, seq_length, epochs):
-    data = np.array(text.strip('\n').split(' '))
-    num_batches = (data.size - 1) // (batch_size * seq_length)
+def create_minibatch(text, batch_size, seq_length, num_epochs):
+    '''
+    Generates mini-batches of data to be processed within the recurrent neural network
 
+    Parameters:
+    -----------
+    text: STR - text corpus
+    batch_size: INT - size of batch
+    seq_length: INT - length of sequence
+    num_epochs: INT - num of epochs for training rnn
+
+    Returns:
+    --------
+    yields input matrix X for a single batch, expected output y, and the current epoch
+    '''
+    data = np.array(text.split(' '))
+    num_batches = data.size // (batch_size * seq_length)
+
+    # Round and reshape data to be even with batch numbers
+    rounded_data = num_batches * batch_size * seq_length
+    x_data = np.reshape(data[0:rounded_data], [batch_size, num_batches * seq_length])
+    y_data = np.reshape(data[1:rounded_data + 1], [batch_size, num_batches * seq_length])
+
+    for epoch in range(num_epochs):
+        for batch in range(num_batches):
+            X = x_data[:, seq_length * batch:seq_length * (batch + 1)]
+            y = y_data[:, seq_length * batch:seq_length * (batch + 1)]
+
+            # Shift by row(s) at each epoch for different looking data
+            X = np.roll(x, -epoch, axis=0)
+            y = np.roll(y, -epoch, axis=0)
+
+            yield X, y, epoch  # Generator
 
 if __name__ == '__main__':
     with open('../data/SOIF/AGameOfThrones.txt') as f:
